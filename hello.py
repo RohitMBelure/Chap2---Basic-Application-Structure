dc8edc83e2bfd3e0817c48f04d1e372687a18514
Rohit <rohitbelure128@gmail.com>
2021-10-05 17:01:24 +0530

    Request Hooks All requests together in one code

--- hello.py
+++ hello.py
@@ -1,15 +1,38 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import session, g
 
-@app.route('/')
-def index():
-	print('index is running')
-	return 'Hello World'
+app = Flask(__name__)
+app.secret_key = 'Rohit@1997'
+
+@app.before_first_request
+def before_request_func():
+	print('This function will run once')
 
 @app.before_request
 def before_request_func():
+	session['foo']='bar'
+	g.username = 'root'
 	print('before_request is running')
-	return 'Intercepted by before_request'
+
+@app.route('/')
+def index():
+	username = g.username
+	foo = session.get('foo')
+	print('index is running', username, foo)
+	return 'Hello World'
+
+@app.after_request
+def after_request_func(response):
+	username = g.username
+	foo = session.get('foo')
+	print('after_request is running',username, foo)
+	return response
+
+@app.teardown_request
+def teardown_request_func(error=None):
+	print('teardown_request is running')
+	if error:
+		print(str(error))
 
 if __name__ == '__main__':
 	app.run(debug=True)
